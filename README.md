8aca6812324e25e17730bbbe4b397def36928219
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-11-22 15:11:42 +0100

    add all files and complete the environment file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Ready_For_Job
 This is a basic questions and other things
+
+This file should help me to remember the basic things about python. I will text a theory and a practice for every necessary thing that I should know how to work with
---
+++ base/0_environment.py
@@ -0,0 +1,27 @@
+# ```
+# environment setting
+# ```
+YOUR_THING = 'literaly anything'
+
+#  how to install necessary python version
+f'sudo apt install {YOUR_THING}'
+f'curl -o- {YOUR_THING} | bash'
+#  how to make virtual environment
+f'for my work poetry is enough'
+f'poetry init to make new project'
+f'add dependencies by poetry add {YOUR_THING}'
+#  how to work with dependencies
+
+#  pip list
+f'this command shows installed packages'
+#  pip freeze
+f'give output of installed packages in requirements.txt format'
+#  pip install/uninstall
+
+'''
+instal packages from:
+PyPI (and other indexes) using requirement specifiers
+VCS project urls
+Local project directories.
+Local or remote source archives.'
+'''
