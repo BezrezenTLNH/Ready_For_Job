8a40a13381fbf6741503dd682b28441488080cc9
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-12-04 22:07:04 +0100

    add new kata solution

--- practice/kats.py
+++ practice/kats.py
@@ -39,9 +39,18 @@
 # def order(sentence):
 #
 #     return ' '.join(sorted(sentence.split(), key=lambda w: sorted(w)))
+#
+# def narcissistic(value):
+#     return sum([int(val)**len(str(value)) for val in str(value)]) == value
 
-def narcissistic(value):
-    return sum([int(val)**len(str(value)) for val in str(value)]) == value
 
+def max_sequence(arr):
+    max_ending_here = max_so_far = 0
 
-print(narcissistic(153))
+    for num in arr:
+        max_ending_here = max(0, max_ending_here + num)
+        max_so_far = max(max_so_far, max_ending_here)
+
+    return max_so_far
+
+print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
