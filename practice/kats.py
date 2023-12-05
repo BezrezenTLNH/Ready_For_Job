e4677448c4b8379b7ac9a6541be0acee3d3f1a05
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-12-05 15:13:48 +0100

    add kata

--- practice/kats.py
+++ practice/kats.py
@@ -44,13 +44,21 @@
 #     return sum([int(val)**len(str(value)) for val in str(value)]) == value
 
 
-def max_sequence(arr):
-    max_ending_here = max_so_far = 0
+# def max_sequence(arr):
+#     max_ending_here = max_so_far = 0
+#
+#     for num in arr:
+#         max_ending_here = max(0, max_ending_here + num)
+#         max_so_far = max(max_so_far, max_ending_here)
+#
+#     return max_so_far
 
-    for num in arr:
-        max_ending_here = max(0, max_ending_here + num)
-        max_so_far = max(max_so_far, max_ending_here)
+def cakes(recipe, available):
+    max_cakes = float('inf')
 
-    return max_so_far
+    for ingredient, amount in recipe.items():
+        if ingredient not in available or available[ingredient] < amount:
+            return 0
+        max_cakes = min(max_cakes, available[ingredient] // amount)
 
-print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+    return max_cakes
