477ef15b6b6532d3e196c1dfa024c57b1d538bc8
BezrezenTLNH <bezrezen97@gmail.com>
2023-12-12 16:15:18 +0100

    change kata

--- practice/kats.py
+++ practice/kats.py
@@ -132,3 +132,20 @@ import string
 # word = "The quick, brown fox jumps over the lazy dog!"
 # print(is_pangram(word))
 
+def find_even_index(arr):
+    result = [n for n, i in enumerate(arr) if sum(arr[n+1:]) == sum(arr[:n])]
+    return min(result) if result else -1
+
+def find_even_index(arr):
+    for i in range(len(arr)):
+        if sum(arr[:i]) == sum(arr[i+1:]):
+            return i
+    return -1
+
+print(find_even_index([1,2,3,4,3,2,1]))
+print(find_even_index([1,100,50,-51,1,1]))
+print(find_even_index([20,10,30,10,10,15,35]))
+print(find_even_index([1,2,3,4,5,6]))
+print(find_even_index([20,10,-80,10,10,15,35]))
+# print(sum([50,-51,1,1]))
+
